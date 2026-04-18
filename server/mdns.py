0498c208d7740629ee7000248120203204f3c662
srkdesign <99723068+srkdesign@users.noreply.github.com>
2026-04-18 17:16:58 +0400

    Fixing crash on Windows

--- server/mdns.py
+++ server/mdns.py
@@ -1,40 +1,47 @@
 import socket
 from zeroconf import ServiceInfo, Zeroconf
+import logging
 
 _zeroconf = None
 _info = None
 
 def advertise(app_name: str, port: int, local_ip: str):
   global _zeroconf, _info
+  try:
+    from zeroconf import ServiceInfo, Zeroconf
+    # convert string to bytes
+    packed_ip = socket.inet_aton(local_ip)
 
-  # convert string to bytes
-  packed_ip = socket.inet_aton(local_ip)
+    _info = ServiceInfo(
+      # standard http service over tcp
+      "_http._tcp.local.",
+      # full service name
+      f"{app_name}._http._tcp.local.",
 
-  _info = ServiceInfo(
-    # standard http service over tcp
-    "_http._tcp.local.",
-    # full service name
-    f"{app_name}._http._tcp.local.",
+      addresses=[packed_ip],
+      port=port,
 
-    addresses=[packed_ip],
-    port=port,
+      properties={
+        "path": "/",
+        "version": "1.0",
+      },
 
-    properties={
-      "path": "/",
-      "version": "1.0",
-    },
+      server=f"{app_name}.local."
+    )
 
-    server=f"{app_name}.local."
-  )
-
-  _zeroconf = Zeroconf()
-  _zeroconf.register_service(_info)
-  print(f"mDNS: https://{app_name}.local:{port}")
+    _zeroconf = Zeroconf()
+    _zeroconf.register_service(_info)
+    print(f"mDNS: https://{app_name}.local:{port}")
+  except Exception as e:
+    logging.warning(f"mDNS is unavailable: {e}")
+    print(f"mDNS is unavailable, use IP address instead")
   return _zeroconf
 
 def stop():
   global _zeroconf, _info
-
-  if _zeroconf and _info:
-    _zeroconf.unregister_service(_info)
-    _zeroconf.close()
+  try:
+    if _zeroconf and _info:
+      _zeroconf.unregister_service(_info)
+      _zeroconf.close()
+  except Exception:
+    pass
