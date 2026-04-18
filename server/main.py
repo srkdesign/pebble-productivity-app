e99a840363990c33ec952588b8c22ac8fad381e9
srkdesign <99723068+srkdesign@users.noreply.github.com>
2026-04-18 17:31:40 +0400

    Changed WSGI server

--- server/main.py
+++ server/main.py
@@ -4,9 +4,18 @@ from server.services.project_service import ensure_default_project
 from server.port import find_free_port, get_local_ip
 from server.certs import get_or_create_cert
 from server.mdns import advertise, stop
-import ssl, threading, webbrowser, time, atexit
-from waitress import create_server
+import ssl, atexit
 from server.app import app
+
+from wsgiref.simple_server import WSGIServer, WSGIRequestHandler
+from socketserver import ThreadingMixIn
+
+class SilentHandler(WSGIRequestHandler):
+  def log_message(self, format, *args):
+    pass
+
+class ThreadedHTTPSServer(ThreadingMixIn, WSGIServer):
+  daemon_threads = True
 
 def start():
   # Init DB
@@ -24,18 +33,26 @@ def start():
   zc = advertise("pebble", port, ip)
   atexit.register(stop)
 
-  # Waitress
+  # SSL Context
   ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-  ssl_ctx.load_cert_chain(cert, key)
+  ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+  ssl_ctx.load_cert_chain(certfile=cert, keyfile=key)
 
-  server = create_server(app, host="0.0.0.0", port=port)
+  # Server
+  server = ThreadedHTTPSServer(("0.0.0.0", port), SilentHandler)
+  server.set_app(app)
   server.socket = ssl_ctx.wrap_socket(server.socket, server_side=True)
 
   url = f"https://localhost:{port}"
   print(f"\n  Local:   {url}")
   print(f"  Network: https://{ip}:{port}\n")
+  print("Press Ctrl/Cmd + C to stop\n")
 
-  server.run()
+  try:
+    server.serve_forever()
+  except KeyboardInterrupt:
+    print("\n Shutting Down...")
+    server.shutdown()
 
 if __name__ == "__main__":
   start()
